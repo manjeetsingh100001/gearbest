a3d55e4809386f134087a119cdfbbac2e46ad6cc
manjeetsingh <manjeetsingh100001@yahoo.com>
2022-02-26 09:50:38 +0530

    project files upload

---
+++ components/footer.js
@@ -0,0 +1,52 @@
+function footer(){
+    return`
+    <div id="navout">
+       <div id="navbrand">
+           <div><img src="https://uidesign.gbtcdn.com/GB/images/promotion/2019/a_evan/Gearbest/logo_gearbest.png?imbypass=true" alt=""></div>
+           <!-- <div><input type="text" id=""></div>
+           <button >Subscribe</button> -->
+       </div>
+   <div id="navinfo">
+       <div>
+           <h4>Company Information</h4>
+           <p>About Us</p>
+           <p>Top Searchs</p>
+           <p>Privacy Policy</p>
+           <p>Terms and Conditions</p>
+           <p>Intellectual Property Policy</p>
+           <p>Testimonials</p>
+           <p>Contact Us</p>
+           <p>Report Security Issue</p>
+           <p>Sell on Gearbest</p>
+           <p> Cookies Policy</p>
+           <p>Tpo Brands</p>
+           <p>GearBest Official Blog</p>
+       </div>
+       <div>
+           <h4>Customer Service</h4>
+           <p>Shipping Information</p>
+           <p> My Favorites</p>
+           <p>Faq For Newsletter Subscription</p>
+           <p>My Points</p>
+           <p>Warranty and Returns</p>
+           <p>Payment Methods</p>
+           <p>Depoist Expansion</p>
+           <p>FAQ & Support</p>
+           <p>Sitemap</p>
+           <p>Gearbest wallet Instruction</p>
+           <p>FAQs about Covid-19</p>
+       </div>
+       <div>
+           <h4>Other Business</h4>
+           <p>Partnership Programs</p>
+           <p> Associate Program</p>
+       </div>
+       <div>
+           <h4>Download app!</h4>
+           <p>Save $3 with App & new user Only</p>
+       </div>
+   </div>
+   </div>
+    
+    `
+}export default footer;
---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+function navbar(){
+    return`
+    <div id="line1">
+        <p>Country Website</p>
+        <p>Language</p>
+        <p>Ship to USD</p>
+      <p>Save $3 with App</p>
+    </div>
+    <div id="line2">
+        <img src="https://uidesign.gbtcdn.com/GB/images/promotion/2019/a_evan/Gearbest/logo_gearbest.png?imbypass=true" alt="">
+        <input type="text" id="it">
+        <p>Sign in</p>
+        <p>Favorites</p>
+        <p>Cart</p>
+    </div>
+    <div id="stline">
+        <div id="stdiv"><p>Category</p></div>
+       <div class="st"> <p>APP ONLY</p></div>
+      <div class="st"> <p>NEW ARRIVALS</p></div>
+    </div>
+    
+    `
+}export default navbar;
